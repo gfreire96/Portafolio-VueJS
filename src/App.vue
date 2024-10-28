9306aa57af8528b858338a9faab94e663e09bc20
Gabriel Freire <gfreire96@gmail.com>
2024-10-28 18:23:23 -0300

    Clase 3 - Barra de Navegación

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <script setup>
-  const nombreApellido = "Gabriel Freire"
-  
+import navbar from './components/navbar.vue';
 </script>
 
 <template>
  <header>
-   <h1> {{ nombreApellido }}</h1>
-   <p>Bienvenido a mi portafolio personal</p>
+  <!-- Barra de Navegación -->
+  <navbar />
+
  </header>
+ <main>
+    <section id="educacion"><h2>Educación - Cursos</h2></section>
+    <section id="experiencia"><h2>Experiencia</h2></section>
+    <section id="proyectos"><h2>Proyectos</h2></section>
+    <section id="habilidades"><h2>Habilidades</h2></section>
+    <section id="intereses"><h2>Intereses</h2></section>
+  </main>
 </template>
 
 <style scoped>
