1f19d179ef57dfeb58e9ead693252f3bd604ed46
Gabriel Freire <gfreire96@gmail.com>
2024-10-30 19:33:48 -0300

    Clase 5 y 6

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import navbar from './components/NavBar.vue';
 import DatosPersonales from './components/DatosPersonales.vue';
+import Educacion from './components/EducacionCursos.vue';
 </script>
 
 <template>
@@ -11,7 +12,7 @@ import DatosPersonales from './components/DatosPersonales.vue';
   <DatosPersonales />
  </header>
  <main>
-    <section id="educacion"><h2>Educación - Cursos</h2></section>
+    <section id="educacion"><h2>Educación - Cursos</h2> <Educacion /> </section>
     <section id="experiencia"><h2>Experiencia</h2></section>
     <section id="proyectos"><h2>Proyectos</h2></section>
     <section id="habilidades"><h2>Habilidades</h2></section>
